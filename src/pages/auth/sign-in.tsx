2f28d436011a042b2aa4ed4dc819b5961bf550e8
WallQ <sergio98@outlook.pt>
2023-05-16 22:04:17 +0100

    Added expor default to auth pages

---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from 'next';
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+const SignIn: NextPage = () => {
+	return (
+		<Fragment>
+			<Head>
+				<title>Pomodoro - Sign In</title>
+			</Head>
+            <h1>Sing In!</h1>
+		</Fragment>
+	);
+};
+
+export default SignIn;
---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from 'next';
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+const SignUp: NextPage = () => {
+	return (
+		<Fragment>
+			<Head>
+				<title>Pomodoro - Sign Up</title>
+			</Head>
+            <h1>Sing Up!</h1>
+		</Fragment>
+	);
+};
+
+export default SignUp;
